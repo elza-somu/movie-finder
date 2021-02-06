a62c304dddebb970952e0f3c63c67a889886f13f
elza-s <elza_somu@yahoo.com>
2021-02-06 16:49:40 -0500

    edited listing & styling

--- src/Search.js
+++ src/Search.js
@@ -34,9 +34,13 @@ export default function Search(){
         <div className="card-list">
           {movies.filter(movie => movie.poster_path).map(movie => (
             <div className="card" key={movie.id}>
-              <img className="card-image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                            alt={movie.title + ' poster'}
-                            />
+              <img className="card-image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}alt={movie.title + ' poster'}/>
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                <p><small>RATING: {movie.vote_average}</small></p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
             </div>
           ))}
         </div>
